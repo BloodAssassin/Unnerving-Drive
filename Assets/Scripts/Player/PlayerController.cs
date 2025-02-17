9b9c7b433b087f795d72b8927a6557ee89d685e2
BoskoJDev <bolle0119@gmail.com>
2025-02-17 12:02:54 +0100

    Added player controls

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,17 +2,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    [SerializeField] float steeringSpeed = 1.0f;
+    [SerializeField] GameObject road;
+    float forwardSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(Keymapping.keyBindings["MoveForward"]))
-        {
-            Debug.Log("W pressed");
-        }
+
+        this.forwardSpeed = Input.GetKey(KeyCode.UpArrow) ? 500.0f : 200.0f;
+        Vector3 pos = road.transform.position;
+        pos.z += this.forwardSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            this.transform.position = SteeringPosition(false);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            this.transform.position = SteeringPosition(true);
+
+        if (pos.z > 75.0f)
+            pos.z = 0.0f;
+
+        road.transform.position = pos;
+    }
+
+    private Vector3 SteeringPosition(bool steerRight)
+    {
+        Vector3 pos = this.transform.position;
+        pos.x += this.steeringSpeed * Time.deltaTime * (steerRight ? -1 : 1);
+        return pos;
     }
 }
